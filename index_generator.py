71b19a8b7391ccc450a1b0268b81df4645170809
freesadness <44709023+freesadness@users.noreply.github.com>
2018-11-23 14:41:52 -0500

    Update index_generator.py

--- index_generator.py
+++ index_generator.py
@@ -7,7 +7,7 @@ def changed(tabs_number, increment):
             index = line.index(change)+len(change)
             newline = list(line)
             if tabs_number < 10:
-                newline[index:index+1]=list(str(tabs_number))
+                newline[index:index+1] = list(str(tabs_number))
             else:
                 newline[index:index+2] = list(str(tabs_number))
             target.write(''.join(newline))
@@ -18,4 +18,23 @@ def changed(tabs_number, increment):
     f.closed
     target.close()
 
-changed(1,2)
+
+def generator_from_blank(tabs_number, increment):
+    end_with = 'type="'
+    f = open('cc.txt', 'r+')
+    target = open('ss.txt', 'w+')
+    for line in f:
+        if end_with in line:
+            index = line.index(end_with)
+            newline = list(line)
+            newline[index - 1:index] = list(' tabindex="' + str(tabs_number) + '" ')
+            target.write(''.join(newline))
+            tabs_number += increment
+            continue
+        else:
+            target.write(str(line))
+    f.closed
+    target.close()
+
+
+generator_from_blank(1, 2)
